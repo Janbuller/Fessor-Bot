9349da19235e90ff7081d6ae0e60039bfb8b7e1e
Konrad Fabritius Christensen <socialneeds@icloud.com>
2017-05-07 15:53:59 +0200

    init Tampermonkey script

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# fessorBot
-<b>Denne bot er ikke ment for XP og er mere eller mindre forladt.</b>
-  Der er en lille chance for at jeg laver en WebKit extension, men det er ikke ret sandsynligt pga. afgangsprøver.
-
-1. Lige nu er der to forskellige scripts. Et til "Tabeleventyr" og et til "De 4 regningsarter", spillene kan findes ved at trykke på 'Spil' knappen øverst på MatematikFessor siden.
-2. Herfra klikker du på det spil du gerne vil botte i. Hvis du vælger "Tabeleventyr" skal du bruge ''tabelEventyr.js'' og hvis du vælger "De 4 regningsarter" skal du bruge ''4RegningsArter.js''.
-3. Vælg her dit niveau og gå videre.
-4. Din side skulle gerne se ca. sådan her ud nu: ![Imgur - Fessor Screenshot](http://i.imgur.com/eLv4nhK.png)
-5. Åben konsolen (Chrome: Ctrl + Shift + I eller Højreklik - undersøg).
-6. Kopier dit valgte script og sæt det ind i konsolen. Tryk enter
-7. Du vil nu blive spurgt fire spørgsmål vedrørende tidsrum mellem handlinger.
-8. Done.
+#fessorBot!
+##Setup
+ 1. Installer TamperMonkey til din browser: 
+	 -  [Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+	 - [Firefox](https://addons.mozilla.org/da/firefox/addon/tampermonkey/)
+	 - [Safari](https://safari.tampermonkey.net/tampermonkey.safariextz)
+	 - [Edge](https://www.microsoft.com/store/apps/9NBLGGH5162S)
+	 - [Opera Next](https://addons.opera.com/en/extensions/details/tampermonkey-beta/)
+ 2. Installer scriptet
+	 1. Tryk [her](https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-code-colors.user.js)
+	 2. Tryk på *Install*
+ 3. Gå til:
+	1. [MatematikFessor](https://matematikfessor.dk/)
+	2. Log in
+	3. Ved startsiden, find menuen *Træning* og vælg *Test dig selv*
+	4. Vælg *5 Skarpe*
+	5. Søg på "Gange med 0"
+	6. Start testen
---
+++ gangeMed0.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         fessorBot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Lav XP i MatematikFessor
+// @author       HarshWombat ( https://github.com/HarshWombat )
+// @match        *www.matematikfessor.dk/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    var pathName = window.location.pathname,
+        loadCheck;
+    if (pathName.match(/^\/test\/fivesharp\/$/)) { //Hvis vi er ved en "fivesharp" test
+        loadCheck = setInterval(function () {
+            if (document.getElementsByClassName("nextButton")[0] && !document.getElementsByClassName("nextButton disabled")[0]) {
+                testLoaded();
+                clearInterval(loadCheck);
+            }
+        }, 100);
+    } else if (pathName.match(/^\/test\/result\/\d+$/)) { //Hvis vi er ved en "fivesharp" test
+        loadCheck = setInterval(function () {
+            if (document.getElementById("recreateTestBtn") && document.getElementsByClassName("evaluateAnswer")[0]) {
+                resultLoaded();
+                clearInterval(loadCheck);
+            }
+        }, 100);
+    }
+})();
+
+function testLoaded() {
+    var i,
+        isTarget = false,
+        loadCheck;
+    for (i = 0; i < document.getElementsByClassName("no-link").length; i += 1) {
+        if (document.getElementsByClassName("no-link")[i].innerHTML === "Gange med 0") {
+            isTarget = true;
+        }
+    }
+    if (isTarget) {
+        console.log(document.getElementsByClassName("nextButton disabled")[0]);
+        while (!document.getElementsByClassName("nextButton disabled")[0]) {
+            document.getElementsByClassName("nextButton")[0].click();
+        }
+        for (i = 0; i < document.getElementsByClassName("answerInput").length; i += 1) {
+            document.getElementsByClassName("answerInput")[i].value = 0;
+        }
+        while (!document.getElementsByClassName("prevButton disabled")[0]) {
+            document.getElementsByClassName("prevButton")[0].click();
+        }
+        document.getElementsByClassName("nextButton")[0].click();
+        document.getElementsByClassName("big actionButton primary")[0].click();
+        loadCheck = setInterval(function () {
+            if (document.getElementsByClassName("big actionButton primary")[1]) {
+                document.getElementsByClassName("big actionButton primary")[1].click();
+                clearInterval(loadCheck);
+            }
+        }, 100);
+    }
+}
+
+function resultLoaded() {
+    var i,
+        isTarget = false,
+        loadCheck;
+    for (i = 0; i < document.getElementsByClassName("no-link").length; i += 1) {
+        if (document.getElementsByClassName("no-link")[i].innerHTML === "Resultat for: Gange med 0") {
+            isTarget = true;
+        }
+    }
+    if (isTarget) {
+        document.getElementById("recreateTestBtn").click();
+        loadCheck = setInterval(function () {
+            document.getElementById("recreateTestBtn").click();
+            if (document.getElementsByClassName("big actionButton primary")[0]) {
+                document.getElementsByClassName("big actionButton primary")[0].click();
+                clearInterval(loadCheck);
+            }
+        }, 100);
+    }
+}
