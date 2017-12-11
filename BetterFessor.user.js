d5a92b9e225e00ec90a457fdbea09435f9df2dd6
Lasse Jensen <lasse@thegamersofpro.com>
2017-12-11 11:58:15 +0100

    Update BetterFessor.user.js

--- BetterFessor.user.js
+++ BetterFessor.user.js
@@ -1,1 +1,24 @@
 
+// ==UserScript==
+// @name         Better-Fessor
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Gøre matematikfessor.dk bedra
+// @author       LaZZe ( https://github.com/Janbuller )
+// @match        https://www.matematikfessor.dk/test/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/Janbuller/Better-Fessor/master/BetterFessor.user.js
+// @downloadURL  https://raw.githubusercontent.com/Janbuller/Better-Fessor/master/BetterFessor.user.js
+// ==/UserScript==
+
+function init() {
+  addCounter()
+}
+
+function addCounter() {
+  var activeQuestion = document.getElementByClassName("questionThumb active")
+  var questionNum = activeQuestion.getAttributeName("data-index")
+  var questionDisp = document.createTextNode(questionNum)
+  var questionText = document.getElementByClassName("questionText")
+  questionText.insertBefore(questionDisp)
+}
