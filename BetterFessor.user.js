7913ff153276428d48185e13f58dadf9e83535e1
Lasse Jensen <lasse@thegamersofpro.com>
2017-12-11 12:05:16 +0100

    Update BetterFessor.user.js

--- BetterFessor.user.js
+++ BetterFessor.user.js
@@ -14,14 +14,14 @@
 function init() {
   if (pathName.match(/^\/test\/fivesharp\/$/)) //Hvis det er en Fivesharp Test
   {
-    addCounter()
+    addCounter();
   }
 }
 
 function addCounter() {
-  var activeQuestion = document.getElementByClassName("questionThumb active")
-  var questionNum = activeQuestion.getAttributeName("data-index")
-  var questionDisp = document.createTextNode(questionNum)
-  var questionText = document.getElementByClassName("questionText")
-  questionText.insertBefore(questionDisp)
+  var activeQuestion = document.getElementByClassName("questionThumb active");
+  var questionNum = activeQuestion.getAttributeName("data-index");
+  var questionDisp = document.createElement("p");
+  var questionText = document.getElementByClassName("questionText");
+  questionText..appendChild(questionDisp);
 }
